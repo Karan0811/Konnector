1869b8d04db1cf251bc8b96d28b01b79b74ae673
Karan0811 <46247163+Karan0811@users.noreply.github.com>
2020-03-30 01:42:43 +0530

    Initial commit

---
+++ routes/api/users.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+
+//@route   Post api/users
+//@desc    register user
+//@access    Public
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Plzz include a valid email').isEmail(),
+    check(
+      'password',
+      'plzz enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // console.log(req.body);
+    res.send('User route');
+  }
+);
+
+module.exports = router;
