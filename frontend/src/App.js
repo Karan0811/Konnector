9e71f423a72c19fdd540852443090c4357445c98
Karan0811 <46247163+Karan0811@users.noreply.github.com>
2020-04-03 16:12:24 +0530

    Add experience and education

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,9 @@ import Alert from './component/layout/Alert';
 import Dashboard from './component/dashboard/Dashboard';
 import PrivateRoute from './component/routing/PrivateRoute';
 import CreateProfile from './component/profile-forms/CreateProfile';
+import Editprofile from './component/profile-forms/EditProfile';
+import AddExperience from './component/profile-forms/AddExperience';
+import AddEducation from './component/profile-forms/AddEducation';
 
 //Redux
 import { Provider } from 'react-redux';
@@ -42,6 +45,21 @@ const App = () => {
                 exact
                 path='/create-profile'
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={Editprofile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
               />
             </Switch>
           </section>
--- frontend/src/component/profile-forms/AddExperience.js
+++ frontend/src/component/profile-forms/AddExperience.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState } from 'react'; //racfp
 import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
@@ -26,8 +26,8 @@ const AddExperience = ({ addExperience, history }) => {
     <Fragment>
       <h1 className='large text-primary'>Add An Experience</h1>
       <p className='lead'>
-        <i className='fas fa-code-branch' /> Add any developer/programming positions
-        that you have had in the past
+        <i className='fas fa-code-branch' /> Add any developer/programming
+        positions that you have had in the past
       </p>
       <small>* = required field</small>
       <form
@@ -123,7 +123,4 @@ AddExperience.propTypes = {
   addExperience: PropTypes.func.isRequired
 };
 
-export default connect(
-  null,
-  { addExperience }
-)(withRouter(AddExperience));
+export default connect(null, { addExperience })(withRouter(AddExperience));
